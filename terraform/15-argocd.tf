6721389b0b3867f2d71c46d8239585bb59e15b6b
Panchanan Panigrahi <panchanandevops@gmail.com>
2024-12-20 14:42:11 +0530

    argocd version updated

--- terraform/15-argocd.tf
+++ terraform/15-argocd.tf
@@ -2,7 +2,7 @@ resource "helm_release" "argocd" {
  name       = "argocd"
  repository = "https://argoproj.github.io/argo-helm"
  chart      = "argo-cd"
- version    = "4.5.2"
+ version    = "7.7.10"
 
  namespace = "argocd"
 
@@ -17,6 +17,6 @@ resource "helm_release" "argocd" {
    name  = "server.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
    value = "nlb"
  }
- 
+
  depends_on = [helm_release.aws_lbc]
 }
