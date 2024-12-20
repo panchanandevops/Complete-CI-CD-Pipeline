ace1c5347267c9ce6a2f1351545e7854232f3d97
Panchanan Panigrahi <panchanandevops@gmail.com>
2024-12-20 13:59:38 +0530

    add argocd helm release

---
+++ terraform/15-argocd.tf
@@ -0,0 +1,22 @@
+resource "helm_release" "argocd" {
+ name       = "argocd"
+ repository = "https://argoproj.github.io/argo-helm"
+ chart      = "argo-cd"
+ version    = "4.5.2"
+
+ namespace = "argocd"
+
+ create_namespace = true
+
+ set {
+   name  = "server.service.type"
+   value = "LoadBalancer"
+ }
+
+ set {
+   name  = "server.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
+   value = "nlb"
+ }
+ 
+ depends_on = [helm_release.aws_lbc]
+}
